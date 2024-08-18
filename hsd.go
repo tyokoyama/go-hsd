9b13f87564acca5f300855efa48b179791ef39c7
Takashi Yokoyama <tksyokoyama@gmail.com>
2024-08-18 15:10:36 +0900

    テストコードを追加。

--- hsd.go
+++ hsd.go
@@ -1,9 +1,12 @@
+// Package hsdはGo言語プログラミングエッセンスのサンプルパッケージです。
 package hsd
 
+// StringDistance returns different character count.
 func StringDistance(lhs, rhs string) int {
 	return Distance([]rune(lhs), []rune(rhs))
 }
 
+// Distance returns different rune count.
 func Distance(a, b []rune) int {
 	dist := 0
 	if len(a) != len(b) {
